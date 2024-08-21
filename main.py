9fff6caffe3cc0756b1625838c3a9a238a640c07
Calvsie <toaofenders@gmail.com>
2024-08-21 11:51:18 -0500

    alignment

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    words = text.split()
+    num_words = get_book_length(text)
     print(text)
-    print(f"{len(words)} words")
+    print(f"{num_words} words")
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_book_length(full_text):
+    words = full_text.split()
+    return len(words)
 
 
 main()
