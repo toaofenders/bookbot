594ca9c37ea77c13dd3bf75c84a9254e27ef0dcc
Calvsie <toaofenders@gmail.com>
2024-08-21 11:17:23 -0500

    word count

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+    words = text.split()
     print(text)
+    print(len(words))
 
 
 def get_book_text(path):
