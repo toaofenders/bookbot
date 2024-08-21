6f5f95299e962b3fe7b2f5fcabd1f190c396e590
Calvsie <toaofenders@gmail.com>
2024-08-21 10:32:53 -0500

    solution alignment adjustment

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
-    with open(books/frankenstein.txt) as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 
 main()
